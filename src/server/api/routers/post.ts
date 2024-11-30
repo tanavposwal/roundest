0d3ceb418f5e799d82d288b67bb85edad13fd7c8
tanavposwal <tanavposwal939@gmail.com>
2024-11-30 23:00:54 +0530

    init

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { posts } from "@/server/db/schema";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
